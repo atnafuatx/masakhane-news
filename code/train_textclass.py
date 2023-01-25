8ab2e054283a23a64574de5cce1c8dc968a2b9db
atanafuatx <atnafuatx@gmail.com>
2023-01-25 01:02:53 -0600

    import pandas

--- code/train_textclass.py
+++ code/train_textclass.py
@@ -24,6 +24,7 @@ import os
 import random
 
 import numpy as np
+import pandas as pd
 import torch
 from torch.utils.data import DataLoader, RandomSampler, SequentialSampler, TensorDataset
 from torch.utils.data.distributed import DistributedSampler
